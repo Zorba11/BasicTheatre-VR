c4aa76837d03e05f9d325fe561cf9b8b22d9e596
Zorba11 <zorbathegreek111@gmail.com>
2020-05-21 05:50:32 -0400

    .animation with sequence, stagger

--- components/scenes/layouts/elements/title.component.js
+++ components/scenes/layouts/elements/title.component.js
@@ -8,39 +8,62 @@ import {
 import { Easing } from 'react-native';
 
 class Title extends React.Component {
-    constructor() {
-        super()
-        this.state = { slideValue: new Animated.Value(2) };
-    }
+  constructor() {
+    super();
+    this.state = {
+      fade: new Animated.Value(0),
+      slide: new Animated.Value(0),
+    };
+  }
 
-    componentDidMount() {
-        Animated.spring(
-          this.state.slideValue,
-          {
-            toValue: 1,
-            friction: 2, //default 7
-            tension: 10 //default 40
-          }
-        ).start();
-    }
+  componentDidMount() {
 
-    render() {
-        return (
-            <View style={{ margin: 0.1, height: 0.5 }}>
-                <Animated.Text style={{
-                    fontSize: 0.5,
-                    fontWeight: '400',
-                    textAlign: 'center',
-                    textAlignVertical: 'center',
-                    transform: [
-                        {scaleY: this.state.slideValue}
-                      ]
-                    }}>
-                    {this.props.text}
-                    </Animated.Text>
-            </View>
-        )
-    }
+    this.state.fade.setValue(1);
+    this.state.slide.setValue(1.5);
+    
+    Animated.sequence([
+      //animation 1
+      Animated.timing(this.state.fade, {
+        toValue: 0,
+        duration: 2000,
+        easing: Easing.ease,
+      }),
+      //fire animation 2 and 3 at same time
+      Animated.stagger(500, [
+        //animation 2
+        Animated.timing(this.state.fade, {
+          toValue: 1,
+          duration: 2000,
+          easing: Easing.ease,
+        }),
+        //animation 3
+        Animated.timing(this.state.slide, {
+          toValue: 0,
+          duration: 2000,
+          easing: Easing.ease,
+        }),
+      ]),
+    ]).start();
+  }
+
+  render() {
+    return (
+      <View style={{ margin: 0.1, height: 0.5 }}>
+        <Animated.Text
+          style={{
+            fontSize: 0.5,
+            opacity: this.state.fade,
+            fontWeight: "400",
+            textAlign: "center",
+            textAlignVertical: "center",
+            transform: [{ translateY: this.state.slide }],
+          }}
+        >
+          {this.props.text}
+        </Animated.Text>
+      </View>
+    );
+  }
 }
 
 export default Title;
